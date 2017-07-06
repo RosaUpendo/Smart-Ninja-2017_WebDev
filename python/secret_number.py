927a4c9118ab4142c04eb3acfe6bea73c1b47f11
Rosa Upendo <rosa.liebe@yahoo.com>
2017-07-06 23:07:00 +0200

    changes in file secret number

--- python/secret_number.py
+++ python/secret_number.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-secret_number = "5"
+secret_number = 5
 guess = raw_input ("Type a number for your chance to win € 100.000.000,- : ")
 
 try:
 
+    guess = int(guess)
     if guess == secret_number:
         print "Üüüüüüiiiii you just won € 100.000.000,- !!"
     else:
         print "Sorry, this was not a lucky guess."
 
-except ValueError in secret_number:
+except ValueError as e:
     print "Only numbers can win."
 
