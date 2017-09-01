d37d995def71f9f5670baab39336d5730580cc0c
RosaUpendo <rosa.liebe@yahoo.com>
2017-09-01 14:36:07 +0200

    added sring lowercase solution

---
+++ python/string_lowercase.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+s = "Today Is A BeautiFul DAY"
+print(s.lower())
+
+x = "Today IS THE MosT BeuTiFuL DaY"
+print(x.lower())
+
+love = raw_input("Insert a love message here and press Enter: ")
+print(love.lower())
