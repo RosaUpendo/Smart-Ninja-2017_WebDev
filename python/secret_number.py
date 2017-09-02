f23522b6f05105e272af1896f4fc961361b014b0
RosaUpendo <rosa.liebe@yahoo.com>
2017-09-02 15:18:10 +0200

    updated secret_number

--- python/secret_number.py
+++ python/secret_number.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import random
 
-secret_number = 5
-guess = raw_input ("Type a number for your chance to win € 100.000.000,- : ")
+def main():
+    secret_number = random.randint(1,100)
 
-try:
 
-    guess = int(guess)
-    if guess == secret_number:
-        print "Üüüüüüiiiii you just won € 100.000.000,- !!"
-    else:
-        print "Sorry, this was not a lucky guess."
+    while True:
+        guess = raw_input("Type a number for your chance to win € 100.000.000,- : ")
+        try:
+            guess = int(guess)
+            if guess == secret_number:
+                print "Üüüüüüiiiii you just won € 100.000.000,- !!"
+                break
+            else:
+                print "Sorry, this was not a lucky guess."
 
-except ValueError as e:
-    print "Only numbers can win."
+        except ValueError as e:
+            print "Only numbers can win."
+
+if __name__ == "__main__":
+    main()
+
 
