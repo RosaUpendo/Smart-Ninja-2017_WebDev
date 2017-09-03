50ee601faad1911036fa00a3708f80b9f0d2358f
RosaUpendo <rosa.liebe@yahoo.com>
2017-09-03 17:00:51 +0200

    changes in file restaurant_menu

--- python/restaurant_menu.py
+++ python/restaurant_menu.py
@@ -2,16 +2,23 @@ print "Welcome to your menu program! <3"
 
 menu = {}
 
-while True:
-    daily_dish = raw_input("Enter a daily dish here: ")
-    price = raw_input("price for daily dish '%s': " % daily_dish)
-    menu[daily_dish] = price
-    print "You added a new daily dish: ", daily_dish, ", the price is ", price, "Euros."
 
-    new = raw_input("Would you like to enter a new dish? (yes/no)")
+daily_dish = raw_input("Enter a daily dish here: ")
+price = raw_input("price for daily dish '%s': " % daily_dish)
+menu[daily_dish] = price
+print "You added a new daily dish: ", daily_dish, ", the price is ", price, "Euros."
 
-    if new.lower() == "no":
-        break
+def yes_no_answere():
+    while True:
+        answere = raw_input("Would you like to enter a new dish? (yes/no)")
+
+        if answere.lower() == "no":
+            return False
+        elif answere.lower() == "yes":
+            return True
+        else:
+            print "This was not valid input. Please enter 'yes' or 'no'."
+trigger = yes_no_answere()
 
 print "On the menu today: %s" % menu
 
@@ -20,8 +27,4 @@ with open("daily_menu.txt", "w+") as menu_file:
     for dish in menu:
         menu_file.write("%s, %s EUR\n" % (dish, menu[dish]))
 
-print "Have a yummy day!"
-
-
-
-
+print "Have a yummy day! Good Bye!"
