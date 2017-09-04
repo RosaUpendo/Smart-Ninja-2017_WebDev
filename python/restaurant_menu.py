58f666fcff6ba1076b258ebc2d3e562c2dbebf71
RosaUpendo <rosa.liebe@yahoo.com>
2017-09-04 12:20:26 +0200

    changes in file restaurant_menu

--- python/restaurant_menu.py
+++ python/restaurant_menu.py
@@ -1,13 +1,3 @@
-print "Welcome to your menu program! <3"
-
-menu = {}
-
-
-daily_dish = raw_input("Enter a daily dish here: ")
-price = raw_input("price for daily dish '%s': " % daily_dish)
-menu[daily_dish] = price
-print "You added a new daily dish: ", daily_dish, ", the price is ", price, "Euros."
-
 def yes_no_answere():
     while True:
         answere = raw_input("Would you like to enter a new dish? (yes/no)")
@@ -18,13 +8,27 @@ def yes_no_answere():
             return True
         else:
             print "This was not valid input. Please enter 'yes' or 'no'."
-trigger = yes_no_answere()
 
-print "On the menu today: %s" % menu
+def main():
+    print "Welcome to your menu program! <3"
 
-with open("daily_menu.txt", "w+") as menu_file:
-    menu_file.write("Our daily menu is cooked with organic ingredients and love <3\n")
-    for dish in menu:
-        menu_file.write("%s, %s EUR\n" % (dish, menu[dish]))
+    menu = {}
+    trigger = True
+    while trigger:
+        daily_dish = raw_input("Enter a daily dish here: ")
+        price = raw_input("price for daily dish '%s': " % daily_dish)
+        menu[daily_dish] = price
+        print "You added a new daily dish: ", daily_dish, ", the price is ", price, "Euros."
+        trigger = yes_no_answere()
 
-print "Have a yummy day! Good Bye!"
+    print "On the menu today: %s" % menu
+
+    with open("daily_menu.txt", "w+") as menu_file:
+        menu_file.write("Our daily menu is cooked with organic ingredients and love <3\n")
+        for dish in menu:
+            menu_file.write("%s, %s EUR\n" % (dish, menu[dish]))
+
+    print "Have a yummy day! Good Bye!"
+
+if __name__ == '__main__':
+    main()
