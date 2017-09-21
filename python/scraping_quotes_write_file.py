09a02c90fe90388900be7536b7c1e8e62a783f35
RosaUpendo <rosa.liebe@yahoo.com>
2017-09-21 12:47:04 +0200

    updated files scaping_quotes...

--- python/scraping_quotes_write_file.py
+++ python/scraping_quotes_write_file.py
@@ -9,8 +9,6 @@ if __name__ == '__main__':
 
     love_quote = soup.findAll("p", attrs={"class": "quoteContent"})
 
-    for love in love_quote:
-        print "<3" * 5, love.text.encode("utf-8"), "<3" * 5
-
-
-
+    with open("love_quotes.txt", "w") as f:
+        for love in love_quote:
+            f.write(love.text.encode("utf-8") + "\n")
